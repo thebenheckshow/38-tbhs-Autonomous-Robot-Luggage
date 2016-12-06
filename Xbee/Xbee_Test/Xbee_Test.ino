d1410fe60732e37711a1e3a00fbe957365e5631b
Technicus <felix@prototype-engineering.com>
2016-12-06 12:29:02 -0600

    Initial Commit

---
+++ Xbee/Xbee_Test/Xbee_Test.ino
@@ -0,0 +1,30 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+ 
+  This example code is in the public domain.
+ */
+
+void setup() {                
+  // initialize the digital pin as an output.
+  // Pin 13 has an LED connected on most Arduino boards:
+  pinMode(13, OUTPUT);
+  Serial.begin(115200);  
+}
+
+void loop() {
+  digitalWrite(13, HIGH);   // set the LED on
+  delay(1000);              // wait for a second
+  digitalWrite(13, LOW);    // set the LED off
+  delay(1000);              // wait for a second
+  
+  Serial.println("TAKE LANTERN");
+
+  digitalWrite(13, HIGH);   // set the LED on
+  delay(1000);              // wait for a second
+  digitalWrite(13, LOW);    // set the LED off
+  delay(1000);              // wait for a second
+  
+  Serial.println("DROP LANTERN");
+
+}
